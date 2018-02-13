590a1c6344c8ca0103191d700b369d407330a9bc
minhlunso1 <aminh1603@gmail.com>
2018-02-13 16:58:58 +0700

    extension_functions

---
+++ app/src/main/java/minhna/android/androidarchitecturecomponent/ui/activity/BaseActivity.kt
@@ -0,0 +1,35 @@
+package minhna.android.androidarchitecturecomponent.ui.activity
+
+import android.app.Activity
+import android.os.Bundle
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.AppCompatActivity
+import android.view.inputmethod.InputMethodManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+/**
+ * Created by minhnguyen on 2/13/18.
+ */
+open class BaseActivity: AppCompatActivity() {
+    lateinit var inputMethodManager: InputMethodManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        inputMethodManager = this.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    }
+
+    inline fun consume(f: () -> Unit): Boolean {
+        f()
+        return true
+    }
+
+    inline fun DrawerLayout.consume(f: () -> Unit): Boolean {
+        f()
+        closeDrawers()
+//        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    fun getThis() = this;
+}
--- app/src/main/java/minhna/android/androidarchitecturecomponent/ui/activity/MainActivity.kt
+++ app/src/main/java/minhna/android/androidarchitecturecomponent/ui/activity/MainActivity.kt
@@ -2,14 +2,22 @@ package minhna.android.androidarchitecturecomponent.ui.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.NavigationView
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 import minhna.android.androidarchitecturecomponent.R
 import minhna.android.androidarchitecturecomponent.ui.fragment.FirstFragment
 import minhna.android.androidarchitecturecomponent.ui.observer.LogObserver
 
-class MainActivity : AppCompatActivity(), LogObserver.Callback {
+class MainActivity : BaseActivity(), LogObserver.Callback, NavigationView.OnNavigationItemSelectedListener {
 
     lateinit var logObserver: LogObserver;
 
@@ -19,8 +27,26 @@ class MainActivity : AppCompatActivity(), LogObserver.Callback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
         logObserver = LogObserver(this.javaClass.simpleName, lifecycle, this)
         lifecycle.addObserver(logObserver)
+
+        val toggle = object : ActionBarDrawerToggle(
+                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
+            override fun onDrawerClosed(v: View?) {
+                super.onDrawerClosed(v)
+            }
+
+            override fun onDrawerOpened(v: View?) {
+                super.onDrawerOpened(v)
+                inputMethodManager.hideSoftInputFromWindow(getThis().currentFocus!!.windowToken, 0)
+            }
+        }
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+        nav_view.setNavigationItemSelectedListener(this)
     }
 
     override fun onStart() {
@@ -31,7 +57,7 @@ class MainActivity : AppCompatActivity(), LogObserver.Callback {
     override fun onResume() {
         super.onResume()
         logObserver.execute()
-        supportFragmentManager.beginTransaction().add(android.R.id.content, FirstFragment()).commit()
+        changeFragment(FirstFragment(), true)
     }
 
     fun changeFragment(f: Fragment, cleanStack: Boolean = false) {
@@ -40,7 +66,7 @@ class MainActivity : AppCompatActivity(), LogObserver.Callback {
             clearBackStack()
         ft.setCustomAnimations(
                 R.anim.abc_fade_in, R.anim.abc_fade_out, R.anim.abc_popup_enter, R.anim.abc_popup_exit)
-        ft.replace(android.R.id.content, f)
+        ft.replace(R.id.main_activity_container, f)
         ft.addToBackStack(null)
         ft.commit()
     }
@@ -52,4 +78,43 @@ class MainActivity : AppCompatActivity(), LogObserver.Callback {
             manager.popBackStack(first.id, FragmentManager.POP_BACK_STACK_INCLUSIVE)
         }
     }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            val fragmentManager = supportFragmentManager
+            if (fragmentManager.backStackEntryCount > 1) {
+                fragmentManager.popBackStack()
+            } else {
+                finish()
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_settings -> {
+                Toast.makeText(this, resources.getText(R.string.not_avail), Toast.LENGTH_SHORT).show()
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.example_1 -> drawer_layout.consume { changeFragment(FirstFragment(), true) }
+        R.id.example_2 -> consume {
+            Toast.makeText(this, resources.getText(R.string.not_avail), Toast.LENGTH_SHORT).show()
+        }
+        else -> super.onOptionsItemSelected(item)
+    }
+
+
+
 }
